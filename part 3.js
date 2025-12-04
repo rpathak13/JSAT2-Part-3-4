143e4da822b54685a61a0982e776542520127189
rpathak13 <rudrapathak07@gmail.com>
2025-12-04 15:45:55 +1100

    created index & 3.1 complete

---
+++ part 3.js
@@ -0,0 +1,12 @@
+// Part 3 
+let myString = "This is a string";
+console.log(myString);
+
+let anotherString = "   Another string";
+console.log(anotherString);
+
+let hello = "Hello there!";
+console.log(hello);
+
+let myName = "Rudra";
+console.log(myName);
